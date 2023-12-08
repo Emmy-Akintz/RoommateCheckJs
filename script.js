f111f05694281cf496346766bfa76e45b86987bf
Ayomide Akintan <emzyakints2005@gmail.com>
2023-12-08 11:03:29 +0100

    changed it to full js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
 const participantInfo = [
     ["Emmanuel", '2203827'],
     ["Miracle", '2203817'],
@@ -5,23 +12,36 @@ const participantInfo = [
     ["Moses", '2203833']
 ]
 
-function isMember(regno) {
+console.log("This is to find out if you're a participant of Dorcas Hall E103.")
+
+rl.question('Enter your reg no: ', (answer) => {
+    const userRegno = answer
+    // console.log(`Here is your reg no ${userRegno}`)
+
+    const index = getParticipantIndex(userRegno)
+
+    if (index !== -1) {
+        const removedUser = participantInfo.splice(index, 1)[0]
+        console.log(`Welcome ${removedUser[0]}`);
+        console.log(`You're a member of Dorcas Hall room E103, and here are your roommates' names:`);
+
+        for (let a = 0; a < participantInfo.length; a++) {
+            console.log(`${participantInfo[a][0]}`);
+        }
+
+        // console.log(`Removed user: ${removedUser}`);
+    } else {
+        console.log(`You're not a member of Dorcas Hall room E103`);
+    }
+
+    rl.close
+})
+
+function getParticipantIndex(regno) {
     for (let i = 0; i < participantInfo.length; i++) {
         if (participantInfo[i][1] === regno) {
-            return true
+            return i
         }
     }
-    return false
-}
-
-alert("This is to find out if you're a participant of Dorcas Hall E103.")
-const userRegno = prompt("Enter your reg number: ")
-document.write(`Here is your reg no ${userRegno}<br>`)
-if (isMember(userRegno)) {
-    document.write(`You're a member of Dorcas Hall room E103 and here are your room mates names:<br>`)
-    for (let a = 0; a < participantInfo.length; a++) {
-        document.write(`${participantInfo[a][0]}<br>`)
-    }
-} else {
-    document.write(`You're not a member of Dorcas Hall room E103.<br>`)
+    return -1
 }
